352746289c0d9f60810d4df5e319415db03e5358
pashu123 <pk5561@gmail.com>
2020-04-08 23:09:47 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,8 @@
+## Dimensionality of Word Embedding
+d_model = 512
+## Multi Head of Attention Layer
+heads = 8
+## Number of Encoder Decoder layers
+num_layers = 6
+## Number of Epochs
+epochs = 10
