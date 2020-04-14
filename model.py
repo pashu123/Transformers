ae5d3e44d7d8eb767c2be54fe98fcd4b3539b19e
pashu123 <pk5561@gmail.com>
2020-04-14 23:17:31 +0530

    refractor

--- model.py
+++ model.py
@@ -88,7 +88,8 @@ class PositionalEncoding(nn.Module):
 
         pe = torch.zeros(max_len, d_model).to(device)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() \
+                            * (-math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
         pe = pe.unsqueeze(0)
