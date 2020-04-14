fa886cbfd8edbaa21d2668aca6ea7478c62e08e3
pashu123 <pk5561@gmail.com>
2020-04-14 09:45:00 +0530

    Added the config

--- chat.py
+++ chat.py
@@ -1,10 +1,14 @@
 import json
 import torch
 from torch.utils.data import Dataset
+import torch.nn.functional as F
 import torch.utils.data
 from models import *
 from utils import *
 import re
+from dataset import vocab_dict
+
+rev_vocab = {v:k for k,v in vocab_dict.items()}
 
 def preprocess_sentence(sentence):
   sentence = sentence.lower().strip()
@@ -19,7 +23,49 @@ def preprocess_sentence(sentence):
   return sentence
 
 
-def evaluate(sentence,model):
+def evaluate(sentence,model,max_len = 40):
+
+    model.eval()
 
     sentence = preprocess_sentence(sentence)
+    sentence = [rev_vocab.get(word,0) for word in sentence]
+    sentence = [rev_vocab['<START>']] + sentence + [rev_vocab['<END>']]
+    sentence = torch.LongTensor(sentence).to(device)
+
+    sentence_mask = (sentence == 0) * 1
+    sentence_mask.unsqueeze(1).unsqueeze(1)
+    sentence_mask = sentence_mask.to(device)
+
+    encoded = model.encoder(sentence,sentence_mask)
+
+    start_word = torch.LongTensor([[rev_vocab['<START>']]]).to(device)
+
+    for i in range(max_len-1):
+
+        size = words.shape[1]
+        target_mask = torch.triu(torch.ones(size, size)).transpose(0, 1).type(dtype=torch.uint8)
+        target_mask = mask.to(device)
+        target_mask = (mask == 0) * 1
+
+        decoded = model.decode(words, encoded, sentence_mask, target_mask)
+        
+        out = model.out(decoded)
+        out = F.softmax(out, dim = -1)
+
+        val, ix = out[:,-1].data.topk(1)
+        next_word = ix[0][0]
+
+        if next_word == rev_vocab['<END>']:
+            break
+
+        words = torch.cat([start_word, torch.LongTensor([next_word])]).to(device)],dim = 1)
+        
+
+
+
     
+
+
+
+
+
